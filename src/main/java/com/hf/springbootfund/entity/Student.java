7d6dc8ce1376b61d0d83f50af909fab418501a22
Hussein <social.101.acc@gmail.com>
2023-10-01 15:53:37 +0300

    Project Setup along the required dependencies

---
+++ src/main/java/com/hf/springbootfund/entity/Student.java
@@ -0,0 +1,25 @@
+package com.hf.springbootfund.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Builder
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(updatable = false,unique = true)
+    private Integer id;
+    @Column(columnDefinition = "varchar(100)")
+    private String FirstName;
+    @Column(columnDefinition = "varchar(100)")
+    private String LastName;
+    @Column(columnDefinition = "varchar(255)")
+    private String Email;
+
+}
---
+++ src/test/java/com/hf/springbootfund/SpringBootFundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hf.springbootfund;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
