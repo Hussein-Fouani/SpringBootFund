5ba49b067ba941cda82613837d747d44cb0eb911
Hussein <social.101.acc@gmail.com>
2023-10-02 15:15:18 +0300

    learned ,configured and applied the concept of spring rest data ,

--- src/main/java/com/hf/springbootfund/Controller/EmployeeController.java
+++ src/main/java/com/hf/springbootfund/Controller/EmployeeController.java
@@ -1,33 +1,14 @@
 package com.hf.springbootfund.Controller;
 
 
-import com.hf.springbootfund.Service.EmployeeServiceJPA;
-import com.hf.springbootfund.entity.Employee;
 import lombok.AllArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @AllArgsConstructor
 @RequestMapping("api")
 public class EmployeeController {
 
-    private EmployeeServiceJPA serviceJPA;
-
-    @GetMapping("/employees")
-    public List<Employee> listemployees() {
-        return serviceJPA.listAllEmployees();
-    }
-
-    @GetMapping("/employees/{employeeId}")
-    public ResponseEntity<Employee> getEmployee(@PathVariable Integer employeeid) {
-
-
-    }
 
 }
--- src/main/java/com/hf/springbootfund/repositories/EmployeeRepo.java
+++ src/main/java/com/hf/springbootfund/repositories/EmployeeRepo.java
@@ -2,6 +2,8 @@ package com.hf.springbootfund.repositories;
 
 import com.hf.springbootfund.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-public interface  EmployeeRepo extends JpaRepository<Employee,Integer> {
+@RepositoryRestResource(path = "members")
+public interface EmployeeRepo extends JpaRepository<Employee, Integer> {
 }
