60bf7a37851309cbb38fb6e398c23f2eb21ad43b
Hussein <social.101.acc@gmail.com>
2023-10-02 16:05:54 +0300

    configured spring security to use the jdbcuserDetailmanager to querybyusername and authority from custom dbs to authenticate

--- src/main/java/com/hf/springbootfund/security/securityConfig.java
+++ src/main/java/com/hf/springbootfund/security/securityConfig.java
@@ -4,16 +4,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class securityConfig {
 
-    @Bean
-    public InMemoryUserDetailsManager userDetailsManager() {
+//    @Bean
+  /*  public InMemoryUserDetailsManager userDetailsManager() {
         UserDetails john = User.builder()
                 .username("john")
                 .password("{noop}12345")
@@ -31,6 +32,14 @@ public class securityConfig {
                 .build();
 
         return new InMemoryUserDetailsManager(john, mary, fouad);
+    }*/
+
+    @Bean
+    public UserDetailsManager manager(DataSource dataSource) {
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager();
+        jdbcUserDetailsManager.setUsersByUsernameQuery("select user_id,pw,active from members where user_id=?");
+        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery("");
+        return jdbcUserDetailsManager;
     }
 
     @Bean
