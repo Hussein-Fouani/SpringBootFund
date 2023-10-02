7192d5cab194d884cf0b89fedfbc2172884e103e
Hussein <social.101.acc@gmail.com>
2023-10-02 12:02:44 +0300

    configured the JPA repo to apply CRUD functionality

--- src/main/java/com/hf/springbootfund/Controller/EmployeeController.java
+++ src/main/java/com/hf/springbootfund/Controller/EmployeeController.java
@@ -1,10 +1,12 @@
-package com.hf.springbootfund.model;
+package com.hf.springbootfund.Controller;
 
 
 import com.hf.springbootfund.Service.EmployeeServiceJPA;
 import com.hf.springbootfund.entity.Employee;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +14,7 @@ import java.util.List;
 
 @RestController
 @AllArgsConstructor
-@RequestMapping("/api")
+@RequestMapping("api")
 public class EmployeeController {
 
     private EmployeeServiceJPA serviceJPA;
@@ -20,6 +22,12 @@ public class EmployeeController {
     @GetMapping("/employees")
     public List<Employee> listemployees() {
         return serviceJPA.listAllEmployees();
+    }
+
+    @GetMapping("/employees/{employeeId}")
+    public ResponseEntity<Employee> getEmployee(@PathVariable Integer employeeid) {
+
+
     }
 
 }
--- src/main/java/com/hf/springbootfund/Service/EmployeeService.java
+++ src/main/java/com/hf/springbootfund/Service/EmployeeService.java
@@ -4,6 +4,15 @@ import com.hf.springbootfund.entity.Employee;
 
 import java.util.List;
 
+
 public interface EmployeeService {
     List<Employee> listAllEmployees();
+
+    void deleteall();
+
+    Employee findEmployeeById(Integer id);
+
+    Employee save(Employee employee);
+
+    void DeleteById(Integer id);
 }
